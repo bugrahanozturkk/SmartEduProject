967bdd1d2b55ab446bf8997e4fa7a8acc5f1540b
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-06-04 23:42:22 +0300

    custom middleware

--- app.js
+++ app.js
@@ -1,15 +1,56 @@
 const express = require('express');
-const pageRoute = require('./routes/pageRoute')
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 const app = express();
 
+//ConnectDB
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfuly');
+  });
+
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+
+//Global Variable
+
+global.userIN = null;
 
 //Middlewares
-app.use(express.static("public"))
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
 
 //Routes
+
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/category', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
