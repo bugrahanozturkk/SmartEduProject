ff5d6f85fee9a0255556a6a460da30d2658dfebf
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-06-07 23:47:35 +0300

    Ready For Heroku

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -40,7 +42,17 @@ app.use(
     store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
   })
 );
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
 
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 //Routes
 
 app.use('*', (req, res, next) => {
@@ -49,7 +61,7 @@ app.use('*', (req, res, next) => {
 });
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
-app.use('/category', categoryRoute);
+app.use('/categories', categoryRoute);
 app.use('/users', userRoute);
 
 const port = 3000;
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,10 +1,21 @@
-exports.getIndexPage = (req, res) => {
-    console.log(req.session.userID);
-    res.status(200).render('index', {
-      page_name: 'index',
-    });
-  };
-  
+const nodemailer = require('nodemailer');
+const Course = require('../models/Course');
+const User = require('../models/User');
+
+exports.getIndexPage = async (req, res) => {
+  const courses = await Course.find().sort('-createdAt').limit(2);
+  const totalCourses = await Course.find().countDocuments();
+  const totalStudents = await User.countDocuments({ role: 'student' });
+  const totalTeachers = await User.countDocuments({ role: 'teacher' });
+
+  res.status(200).render('index', {
+    page_name: 'index',
+    courses,
+    totalCourses,
+    totalStudents,
+    totalTeachers,
+  });
+};
 
 exports.getAboutPage = (req, res) => {
   res.status(200).render('about', {
@@ -22,4 +33,59 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+  try {
+    const outputMessage = `
+  
+  <h1> Message Details</h1>
+  <ul>
+    <li>
+      Name: ${req.body.name}
+    </li>
+    <li>
+      Email: ${req.body.email}
+    </li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+  `;
+
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'bugrahanoz061@gmail.com',
+        pass: 'okmpscygqerwrbfb',
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form 👻" <bugrahanoz061@gmail.com>',
+      to: 'bugrahanoz611@gmail.com',
+      subject: 'Hello ✔',
+      text: 'Smart EDU Contact Form New Message',
+      html: outputMessage,
+    });
+
+    console.log('Message sent: %s', info.messageId);
+
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    req.flash('success', 'We received your message succesfully.');
+
+    res.status(200).redirect('contact');
+  } catch (err) {
+    //req.flash('error', `Something happened! ${err}`);
+    req.flash('error', `Something happened!`);
+    res.status(200).redirect('contact');
+  }
 };
